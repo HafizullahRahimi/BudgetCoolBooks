21867ba8bbb311bb73cb66887626c5703db8605d
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2023-04-06 13:05:54 +0200

    Add project files.

---
+++ BudgetCoolBooks_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BudgetCoolBooks_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
