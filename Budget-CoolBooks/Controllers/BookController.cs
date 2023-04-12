32d7c8143f6fb385d455a3beb9df95cbaf23a5ec
ToBeOtt <119058608+ToBeOtt@users.noreply.github.com>
2023-04-12 11:01:18 +0200

    added booklist in books

---
+++ Budget-CoolBooks/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Budget_CoolBooks.Services.Books;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budget_CoolBooks.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookServices _bookServices;
+
+        public BookController(BookServices bookServices)
+        {
+            _bookServices = bookServices;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _bookServices.GetAllBooksSorted();
+            if (result == null)
+            {
+                return NotFound();
+            }
+            ViewBag.bookListSorted = result;
+            return View(ViewBag.bookListSorted);
+        }
+    }
+}
--- Budget-CoolBooks/Services/Reviews/ReviewServices.cs
+++ Budget-CoolBooks/Services/Reviews/ReviewServices.cs
@@ -17,24 +17,17 @@ namespace Budget_CoolBooks.Services.Reviews
         public async Task<List<Review>> GetReviewByUsername(string userName)
         {
             // Include navigation-property. Sorts out all username that has IsDeleted=true. Sort by last created.
-                    return _context.Reviews
-                        .Include(r => r.User)
-                        .Where(r => r.User.UserName == userName && !r.IsDeleted)
-                        .OrderByDescending(r => r.Created) 
-                        .ToList();
+                    return _context.Reviews.Include(r => r.User).Where(r => r.User.UserName == userName && !r.IsDeleted)
+                        .OrderByDescending(r => r.Created).ToList();
         }
         public async Task<Review> GetReviewByID(int id)
         {
-            return _context.Reviews
-                .Where(r => r.Id == id)
-                .FirstOrDefault();
+            return _context.Reviews.Where(r => r.Id == id && !r.IsDeleted).FirstOrDefault();
         }
         public async Task<bool> DeleteReview(Review review)
         {
             review.IsDeleted = true;
-            var result =
-            _context.Reviews
-            .Update(review);
+            var result =_context.Reviews.Update(review);
             return Save();
         }
 
